75d5a97837b098c3891a748ed1759852d3abdaf4
Terckoer <pv17021353@vallarta.tecmm.edu.mx>
2022-02-04 13:35:46 -0600

    Metodos 1

--- app.js
+++ app.js
@@ -102,43 +102,63 @@
 
 // BOM BROWSER OBJECT MODEL
 
-document.addEventListener('click', (e)=>{
-    console.log(e.target);
+// document.addEventListener('click', (e)=>{
+//     console.log(e.target);
+// });
+
+// window.addEventListener('resize', (e)=>{
+//     console.clear('Mouse move en window');
+//     console.log('Se mueve el mouse'+e);
+//     console.log(this.innerHeight);
+//     console.log(this.innerWidth);
+//     console.log(this.outerHeight);
+//     console.log(this.outerWidth);
+//     console.log(window.screenX);
+//     console.log(window.screenY);
+
+
+
+// });
+
+// window.addEventListener('DOMContentLoaded', (e)=>{
+//     console.log(e);
+// });
+
+// window.addEventListener('load', (e)=>{
+//     console.log(e);
+// });
+
+
+// this.addEventListener('DOMContentLoaded', (e)=>{
+//     console.log(e);
+// });
+
+// this.addEventListener('load', (e)=>{
+//     console.log(e);
+// });
+
+// document.addEventListener('DOMContentLoaded', (e)=>{
+//     console.log(e);
+// });
+
+// METODOS 
+
+const $abrir = document.querySelector('.abrir');
+const $cerrar = document.querySelector('.cerrar');
+const $imprimir = document.querySelector('.imprimir');
+
+let referenciaVentana;
+
+$abrir.addEventListener('click',(e)=>{
+    //referenciaVentana = open('http://www.youtube.com');
+    referenciaVentana = open();
 });
 
-window.addEventListener('resize', (e)=>{
-    console.clear('Mouse move en window');
-    console.log('Se mueve el mouse'+e);
-    console.log(this.innerHeight);
-    console.log(this.innerWidth);
-    console.log(this.outerHeight);
-    console.log(this.outerWidth);
-    console.log(window.screenX);
-    console.log(window.screenY);
-
-
-
+$cerrar.addEventListener('click',(e)=>{
+    referenciaVentana.close();
 });
 
-window.addEventListener('DOMContentLoaded', (e)=>{
-    console.log(e);
+$imprimir.addEventListener('click',(e)=>{
+    window.print();
 });
-
-window.addEventListener('load', (e)=>{
-    console.log(e);
-});
-
-
-this.addEventListener('DOMContentLoaded', (e)=>{
-    console.log(e);
-});
-
-this.addEventListener('load', (e)=>{
-    console.log(e);
-});
-
-document.addEventListener('DOMContentLoaded', (e)=>{
-    console.log(e);
-});
-
 
