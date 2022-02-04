018db65d31b43cf4eb4a6d62c4ff3f7a422ca16d
Terckoer <pv17021353@vallarta.tecmm.edu.mx>
2022-02-03 21:31:13 -0600

    preventDefault

--- app.js
+++ app.js
@@ -1,89 +1,100 @@
 
-// const fakeApiItems = 
-//     [
-//         {
-//             url:'https://placeimg.com/240/240/people',
-//             title:'people'
-//         },
-//         {
-//             url:'https://placeimg.com/240/240/animals',
-//             title:'animals'
-//         },
-//         {
-//             url:'https://placeimg.com/240/240/tech',
-//             title:'tech'
-//         }
-//     ];
+// // const fakeApiItems = 
+// //     [
+// //         {
+// //             url:'https://placeimg.com/240/240/people',
+// //             title:'people'
+// //         },
+// //         {
+// //             url:'https://placeimg.com/240/240/animals',
+// //             title:'animals'
+// //         },
+// //         {
+// //             url:'https://placeimg.com/240/240/tech',
+// //             title:'tech'
+// //         }
+// //     ];
 
-// const $container = document.getElementById('id-container');
-// const $template = document.getElementById('id-template').content;
-// const $fragment = document.createDocumentFragment();
+// // const $container = document.getElementById('id-container');
+// // const $template = document.getElementById('id-template').content;
+// // const $fragment = document.createDocumentFragment();
 
-// fakeApiItems.forEach(el =>{
-//     $template.querySelector('img').setAttribute('src', el.url);
-//     $template.querySelector('p').textContent = el.title;
-//     const $clone = document.importNode($template, true);
-//     $fragment.appendChild($clone);
-// });
-
-
-// $template.querySelector('img').setAttribute('src', fakeApiItems[0].url);
-// $template.querySelector('p').textContent = fakeApiItems[0].title+'aaaa';
-// const $clone = document.importNode($template, true);
+// // fakeApiItems.forEach(el =>{
+// //     $template.querySelector('img').setAttribute('src', el.url);
+// //     $template.querySelector('p').textContent = el.title;
+// //     const $clone = document.importNode($template, true);
+// //     $fragment.appendChild($clone);
+// // });
 
 
-// $container.appendChild($fragment);
-
-// $container.replaceChild($clone, $container.lastChild);
-// $container.insertBefore($clone, $container.children[2]);
-// $container.insertBefore($clone, $container.children[2]);
-
-// const $elementCreated = document.createElement('div');
-// const $elementCreated1 = document.createElement('img');
-// const $elementCreated2 = document.createElement('p');
-// $elementCreated1.setAttribute('src', 'https://placeimg.com/320/320/any');
-// $elementCreated2.textContent = 'This was created in js';
-// $elementCreated.appendChild($elementCreated1);
-// $elementCreated.appendChild($elementCreated2);
-
-//$container.after($elementCreated);
-//$container.before(document.importNode($elementCreated, true));
-// $container.prepend(document.importNode($elementCreated, true));
-// $container.append(document.importNode($elementCreated, true));
+// // $template.querySelector('img').setAttribute('src', fakeApiItems[0].url);
+// // $template.querySelector('p').textContent = fakeApiItems[0].title+'aaaa';
+// // const $clone = document.importNode($template, true);
 
 
-const $div = document.getElementById('id-div');
-const enter = function () {
-    alert('Enter');
-}
+// // $container.appendChild($fragment);
 
-const leave = function () {
-    alert('Leave');
-}
+// // $container.replaceChild($clone, $container.lastChild);
+// // $container.insertBefore($clone, $container.children[2]);
+// // $container.insertBefore($clone, $container.children[2]);
+
+// // const $elementCreated = document.createElement('div');
+// // const $elementCreated1 = document.createElement('img');
+// // const $elementCreated2 = document.createElement('p');
+// // $elementCreated1.setAttribute('src', 'https://placeimg.com/320/320/any');
+// // $elementCreated2.textContent = 'This was created in js';
+// // $elementCreated.appendChild($elementCreated1);
+// // $elementCreated.appendChild($elementCreated2);
+
+// //$container.after($elementCreated);
+// //$container.before(document.importNode($elementCreated, true));
+// // $container.prepend(document.importNode($elementCreated, true));
+// // $container.append(document.importNode($elementCreated, true));
 
 
-//$div.onmouseenter = enter;
+// const $div = document.getElementById('id-div');
+// const enter = function () {
+//     alert('Enter');
+// }
 
-// $div.addEventListener('click', ()=>{
-//   alert('Presione el click');  
-// });
-// const $id = document.getElementById('id-div');
-// const $id2 = document.getElementById('id-div2');
-// function saluda(nombre='perengano'){
-//     alert('Hola '+nombre);
+// const leave = function () {
+//     alert('Leave');
 // }
 
 
-// $id.addEventListener('click', function(e){
-//     saluda('Fulano');
+// //$div.onmouseenter = enter;
+
+// // $div.addEventListener('click', ()=>{
+// //   alert('Presione el click');  
+// // });
+// // const $id = document.getElementById('id-div');
+// // const $id2 = document.getElementById('id-div2');
+// // function saluda(nombre='perengano'){
+// //     alert('Hola '+nombre);
+// // }
+
+
+// // $id.addEventListener('click', function(e){
+// //     saluda('Fulano');
+// // });
+
+
+// // const remove = (e) =>{
+// //     alert('R');
+// //     $id2.removeEventListener('click', remove);
+// // }
+
+
+// // $id2.addEventListener('click', remove);
+
+
+// const $divs = document.querySelectorAll('div');
+
+// $divs.forEach(e =>{
+//     e.addEventListener('click', (evt)=>{
+//         alert('this');
+//         evt.stopPropagation();
+//         evt.preventDefault();
+//         console.log(e.className);
+//     });
 // });
-
-
-// const remove = (e) =>{
-//     alert('R');
-//     $id2.removeEventListener('click', remove);
-// }
-
-
-// $id2.addEventListener('click', remove);
-
